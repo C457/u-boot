eaea7e7e679e1ceebdcebe5b1424566d5e382645
c457 <android.c357@gmail.com>
2025-02-08 00:52:42 -0600

    u-boot: Update build script for cm-11

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export ARCH=arm
 # This is user specific
-export CROSS_COMPILE=~/android/kia/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=~/android/cm-11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
 make clean
 make tcc897x-lcn_defconfig
 make
